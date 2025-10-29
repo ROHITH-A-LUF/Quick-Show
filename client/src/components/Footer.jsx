b32249c5032025b130626f0d4d0ea21f6331a918
ROHITH-A-LUF <btwsuryastuffs@gmail.com>
2025-10-29 14:32:20 +0530

    Changes in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div className="md:max-w-96">
                     <img className="w-36 h-auto" src={assets.logo} alt="logo" />
                     <p className="mt-6 text-sm">
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        Get into the world of movies with QuickShow. Your ultimate destination for movie reviews, ratings, and recommendations.
                     </p>
                     <div className="flex items-center gap-2 mt-4">
                         <img src={assets.googlePlay} alt="google play" className="h-9 w-auto" />
@@ -28,14 +28,16 @@ const Footer = () => {
                     <div>
                         <h2 className="font-semibold mb-5">Get in touch</h2>
                         <div className="text-sm space-y-2">
-                            <p>+1-234-567-890</p>
-                            <p>contact@example.com</p>
+                            <p>+91 93458 79806</p>
+                            <p>+91 76039 41513</p>
+                            <p>suryakumar2k05@gmail.com</p>
+                             <p>ayyadurairohith941@gmail.com</p>
                         </div>
                     </div>
                 </div>
             </div>
             <p className="pt-4 text-center text-sm pb-5">
-                Copyright {new Date().getFullYear()} © GreatStack. All Right Reserved.
+                Copyright {new Date().getFullYear()} © QuickShow. All Right Reserved.
             </p>
         </footer>
   )
